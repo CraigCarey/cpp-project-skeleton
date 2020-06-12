0ce0169291097a97420381a05b3f277961aecc86
Craig <craigc@anyvision.co.uk>
2020-06-12 14:56:27 +0100

    Added private boost dependency, not really happy with how imports are exported though

--- libskeleton/src/skeleton.cpp
+++ libskeleton/src/skeleton.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 
-#include "Poco/DigestStream.h"
-#include "Poco/MD5Engine.h"
+#include <Poco/DigestStream.h>
+#include <Poco/MD5Engine.h>
+
+#include <boost/regex.hpp>
 
 namespace skeleton {
 
@@ -12,6 +14,9 @@ std::string get_md5_sum()
     Poco::MD5Engine md5;
     Poco::DigestOutputStream ds(md5);
     ds << "abcdefghijklmnopqrstuvwxyz";
+
+    // Arbitrary internal boost use
+    boost::regex expr{R"(\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b)"};
 
     return std::string(Poco::DigestEngine::digestToHex(md5.digest()));
 }
